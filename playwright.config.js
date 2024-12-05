c366d639dd07ebb47a6cb5e8746cf8bcfb83946a
zhouhuajian <zhouhuajian@company.com>
2024-12-05 17:55:27 +0800

    08 使用 Playwright Test 扩展

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
-      slowMo: 5000
+      // slowMo: 5000
     }
   },
 
---
+++ tests/feedback-page.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('测试页面基础', async ({ page }) => {
+  await page.goto('http://localhost/feedback');
+  await expect(page.getByRole('heading')).toContainText('用户反馈');
+  await expect(page.getByText('（接收处理结果）')).toBeVisible();
+  await expect(page.locator('input[name="email"]')).toHaveValue('user@email.com');
+});
