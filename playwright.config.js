23329c8d08a2da71eef47827d455dec011eb2772
zhouhuajian <zhouhuajian@company.com>
2024-12-11 16:56:59 +0800

    09 Playwright Test 配置

--- playwright.config.js
+++ playwright.config.js
@@ -17,21 +17,25 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', { 'open': 'never' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-
+    baseURL: 'http://localhost',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
       // slowMo: 5000
-    }
+    },
+    headless: false
   },
 
   /* Configure projects for major browsers */
--- tests/feedback-page.spec.js
+++ tests/feedback-page.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('测试页面基础', async ({ page }) => {
-  await page.goto('http://localhost/feedback');
+  await page.goto('/feedback');
   await expect(page.getByRole('heading')).toContainText('用户反馈');
   await expect(page.getByText('（接收处理结果）')).toBeVisible();
   await expect(page.locator('input[name="email"]')).toHaveValue('user@email.com');
